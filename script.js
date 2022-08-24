6411f412f8a242a3dbbfa24fde208c69984d1d7d
Your Name <you@example.com>
2022-08-24 17:54:32 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const data = {
+    color: 'red',
+    width: 400,
+    height: 500
+}
+// function func(options) {
+//     let color = options.color;
+//     let width = options.width;
+//     let height = options.height
+
+//     console.log(color,width,height);
+// }
+
+let {color,width,height} = data
+
+console.log(color,width,height);
